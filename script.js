c8e49faac87e4dc19bd644ebd56c988ab05b0ec0
Ramesh Sannareddy <rsannareddy@gmail.com>
2021-04-21 01:57:01 +0000

    commit apr 20,2021

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var ys = document.getElementById("years").value;
+    var i = p*ys*r/100;
+    var y = new Date().getFullYear()+parseInt(ys);
+    document.getElementById("result").innerHTML = "<br\>If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of <mark>"+r+"%</mark>\<br\>You will receive an amount of <mark>"+i+"</mark>,\<br\>in the year <mark>"+y+"</mark>\<br\>"
+
 }
-        
+function updaterate()
+{
+    var rv = document.getElementById("rate").value;    
+    document.getElementById("rate_val").innerText=rv+"%";
+}
+function validate_p(){
+    var p =document.getElementById("principal").value;
+    var response
+    if(p<=0){
+        response=alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+    
+}       
